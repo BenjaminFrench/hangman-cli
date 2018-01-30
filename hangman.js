345ea43688edd1fe5869403054f8ea46dd1a1295
Ben French <ben.jminw@gmail.com>
2018-01-30 15:53:27 -0700

    Adding askForGuess and guessLetter functions.

--- hangman.js
+++ hangman.js
@@ -5,6 +5,11 @@ var Word = require('./word.js');
 // Array for word objects
 var wordlist = [];
 
+var currentWord;
+var currentWordStr;
+var progress = '';
+var guessesRemaining;
+
 // Read wordlist from file. Words should be on their own line
 fs.readFile("./wordlist.txt", 'utf-8', function(error, data) {
     if (error) throw error;
@@ -17,12 +22,12 @@ fs.readFile("./wordlist.txt", 'utf-8', function(error, data) {
 });
 
 function startGame() {
-    var currentWord;
-    var currentWordStr;
-    var progress = '';
+    guessesRemaining = 25;
     // choose a random word from the list
-    currentWord = wordlist[Math.floor(Math.random() * wordlist.length)]
+    currentWord = wordlist[Math.floor(Math.random() * wordlist.length)];
     currentWordStr = currentWord.asString();
+
+    // Initialize progress string
     for (char of currentWordStr) {
         if (char !== ' ') {
             progress += '_';
@@ -32,6 +37,44 @@ function startGame() {
         }
     }
     console.log(stringWithSpaces(progress));
+    console.log();
+    askForGuess();
+}
+
+function askForGuess(params) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'letterGuessed',
+            message: 'Guess a letter:',
+            validate: function (value) {
+                var pass = value.match(
+                    /^([A-z])$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return 'Please enter a single character';
+            }
+        }
+    ])
+    .then(answers => {
+        guessLetter(answers.letterGuessed);
+        console.log(stringWithSpaces(progress));
+    });
+}
+
+function guessLetter(char) {
+    var occurences = currentWord.indexesOf(char);
+    if (occurences.length > 0) {
+        occurences.forEach(element => {
+            progress[element] = currentWordStr[element];
+        });
+
+    }
+    else {
+
+    }
 }
 
 // Print a string with spaces between each character
