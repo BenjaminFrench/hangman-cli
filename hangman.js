dd9c5ef7e704045012ea32bcd242c05228f5ad9a
Ben French <ben.jminw@gmail.com>
2018-01-30 11:32:27 -0700

    Reading wordlist from file and making array of word objects from it.

--- hangman.js
+++ hangman.js
@@ -1,1 +1,16 @@
 var inquirer = require('inquirer');
+var fs = require('fs');
+var Word = require('./word.js');
+
+// Array for word objects
+var wordlist = [];
+
+// Read wordlist from file. Words should be on their own line
+fs.readFile("./wordlist.txt", 'utf-8', function(error, data) {
+    if (error) throw error;
+    words = data.split('\n');
+
+    words.forEach(element => {
+        wordlist.push(new Word(element));
+    });
+});
